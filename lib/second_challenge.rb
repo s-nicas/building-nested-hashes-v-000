a3bff33f789e0614506ef0b5f47be4ba71a31a7e
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-04-25 22:48:39 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,13 +1,18 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
-      :patriarch => { :name => "Lord Montague":age => "53"},
-      :matriarch => {:name=> "Lady Montague", :age => "54" },
-      :hero => { :name =>
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
+ epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
 
   
 end
