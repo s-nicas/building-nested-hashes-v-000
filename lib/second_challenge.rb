80403441afda3ad4cd965978b758863cec90503b
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-04-25 22:46:32 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,8 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      
+      :patriarch => { :name => "Lord Montague":age => "53"},
+      :matriarch => {:name=> "Lady Montague", :age =>
       }, 
     :capulet => {
       #your key/value pairs here!
